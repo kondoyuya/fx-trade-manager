b6fe44af3e038cc2f95469f38b4a0f277b2db292
yuya.kondo <halcyonvgc@icloud.com>
2025-11-11 05:28:52 +0900

    クエリの修正

--- src-tauri/src/db/queries/trades.rs
+++ src-tauri/src/db/queries/trades.rs
@@ -6,7 +6,7 @@ use crate::models::db::trade::Trade;
 pub fn insert_trade(state: &DbState, trade: Trade) -> Result<(), String> {
     let state = state.conn.lock().map_err(|e| e.to_string())?;
     state.execute(
-        "INSERT INTO OR IGNORE trades (
+        "INSERT OR IGNORE INTO trades (
         pair, side, lot, entry_rate, exit_rate, entry_time, exit_time, profit, profit_pips, swap, memo
         ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11)",
         params![
