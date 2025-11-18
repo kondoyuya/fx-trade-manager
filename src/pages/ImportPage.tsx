a62740818843be0c19b6d9f244553b285cdc2ca0
yuya.kondo <halcyonvgc@icloud.com>
2025-11-18 19:12:41 +0900

    ローソク足インポートをできるようにする

--- src/pages/ImportPage.tsx
+++ src/pages/ImportPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import RecordUploader from '../components/RecordUploader.tsx'
-// import CandleUploader from "../components/CandleUploader.tsx";
+import CandleUploader from "../components/CandleUploader.tsx";
 
 const HistoryPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <RecordUploader />
-      {/* <CandleUploader /> */}
+      <CandleUploader />
     </div>
   )
 }
